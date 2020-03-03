f6cc35fadb870cc3b40984f01f2291c4f80f6ef7
arsendr <51822852+arsendr@users.noreply.github.com>
2020-03-03 11:24:02 +0100

    Test add payment
    now has documentation

--- test/TestAddPayment.java
+++ test/TestAddPayment.java
@@ -4,6 +4,12 @@ import static org.junit.Assert.*;
 import controllayer.*;
 import modellayer.Currency;
 
+/**
+* Tests conducted by UCN Denmark, Computer Science students of group number 5
+* Test conducted in this class, checks if the most important method of this program, AddPayment is behaving as expected.
+* To verify it behaves as expected, 1 euro is tested to display 40 minutes of parking time.
+*/
+
 public class TestAddPayment {
 	ControlPayStation ps;
 	
@@ -12,15 +18,20 @@ public class TestAddPayment {
 		ps = new ControlPayStation();
 	}
 
+	
+	/**
+	 * Entering 1 euro should display 40 minutes of parking time.
+	 */
 	@Test
 	public void shouldAddPaymentwithEUR() throws IllegalCoinException{
-		
-		int amount = 1;
-		int expectedParkingTime = 40;
+		//Arrange
+		int amount = 1;  //euro
+		int expectedParkingTime = 40;  //minutes
 		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
 		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		//Act
 		ps.addPayment(amount, coinCurrency, coinType);
-		
+		//Assert
 		assertEquals("Should add payment of 1 eur succesfully: ", expectedParkingTime, ps.readDisplay());
 	}
 	
