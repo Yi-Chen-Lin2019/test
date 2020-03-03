04146959fbaac620d535479feac6a593e5b34ab4
arsendr <51822852+arsendr@users.noreply.github.com>
2020-03-03 11:24:46 +0100

    test for adding both currencies
    now has documentation

--- test/TestAddPaymentWithBothCu.java
+++ test/TestAddPaymentWithBothCu.java
@@ -4,6 +4,11 @@ import static org.junit.Assert.*;
 import controllayer.*;
 import modellayer.Currency;
 
+/**
+ * Tests conducted by UCN Denmark, Computer Science students of group number 5
+ * Test conducted in this class, checks if system handles expected parking time correctly when entering both currencies of euro and danish kronas.
+ */
+
 public class TestAddPaymentWithBothCu {
 	ControlPayStation ps;
 	
@@ -12,23 +17,29 @@ public class TestAddPaymentWithBothCu {
 		ps = new ControlPayStation();
 	}
 
+	
+	/**
+	 * Entering 1 euro should display 40 minutes of parking time, while also entering 10 kronas, should display 54 minutes of parking time.
+	 * Together both currencies should display total of 94 minutes of parking time.
+	 */
 	@Test
 	public void shouldAddPaymentwithBOTH() throws IllegalCoinException{
-		
-		int amount = 1;
-		int expectedParkingTime = 40;
+		//Arrange
+		int amount = 1;  //euro
+		int expectedParkingTime = 40;  //minutes
 		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
 		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
 		ps.addPayment(amount, coinCurrency, coinType);
 		
-		int amount1 = 10;
-		int expectedParkingTime1 = 54;
+		int amount1 = 10; //kronas
+		int expectedParkingTime1 = 54; //minutes
 		Currency.ValidCurrency coinCurrency1 = Currency.ValidCurrency.DKK;
 		Currency.ValidCoinType coinType1 = Currency.ValidCoinType.INTEGER;
+		//Act
 		ps.addPayment(amount1, coinCurrency1, coinType1);
 		
 		int total = expectedParkingTime+expectedParkingTime1;
-		
+		//Assert
 		assertEquals("Should be 94: ", total, ps.readDisplay());
 	}
 	
