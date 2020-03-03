eeacaad101259a18613b899a2cd308e4db4701d0
Yi-Chen-Lin2019 <58112802+Yi-Chen-Lin2019@users.noreply.github.com>
2020-03-03 09:44:19 +0100

    update

--- test/TestIllegalCoin.java
+++ test/TestIllegalCoin.java
@@ -1,12 +1,12 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.function.*;
 import java.lang.reflect.Executable;
 
 import org.junit.*;
+import org.junit.jupiter.api.Assertions;
 
 import controllayer.*;
 import modellayer.Currency;
@@ -31,38 +31,97 @@ public class TestIllegalCoin {
 	 */
 	
 	// Norwegian coin
-	@Test(expected = IllegalCoinException.class)
+	@Test
 	public void shouldRejectIllegalCurrencyNokCoin() throws IllegalCoinException {
 		// Arrange
-		int expectedParkingTime = 3;	// In minutes
 		int coinValue = 5;
 		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.NOK;
 		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
-		// Act
-		ps.addPayment(coinValue, coinCurrency, coinType);
-					
+		
 		// Assert
-		assertEquals("NOK is invalid coins", coinValue, 0);
+		Exception exception = assertThrows(IllegalCoinException.class, () ->
+			// Act
+			ps.addPayment(coinValue, coinCurrency, coinType));
+			
 	}
-	// unknown Euro coin value
-	@Test(expected = IllegalCoinException.class)
-	public void shouldRejectIllegalEuroCoin() throws IllegalCoinException {
+	// SEK
+	@Test
+	public void shouldRejectIllegalCurrencySekCoin() throws IllegalCoinException {
 		//Arrange
-		int expectedParkingTime = 3;	// In minutes
 		int coinValue = 5;
 		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.SEK;
 		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
-		// Act
-		ps.addPayment(coinValue, coinCurrency, coinType);
-					
-		// Assert
-		assertEquals("Invalid coins", coinValue, 0);
+		
+		//Assert
+		Assertions.assertThrows(IllegalCoinException.class, new org.junit.jupiter.api.function.Executable()
+        {
+             @Override
+             public void execute() throws Throwable
+             {
+            	// Act
+ 				ps.addPayment(coinValue, coinCurrency, coinType);
+             }
+        });	
 	}
-	//@Test
-	//public void testIllegalCoinException() {	
-		//Exception exception = assertThrows(IllegalCoinException.class, () ->
-		//sc.test2();
-		// Maybe also verify that stock is unchanged...
+	// unknown Euro coin value
+	@Test
+	public void shouldRejectIllegalEuroCoin() throws IllegalCoinException {
+		//Arrange	
+		int coinValue = 30;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
 			
-	//}
+		// Assert
+		Exception exception = assertThrows(IllegalCoinException.class, () ->
+			// Act	
+			ps.addPayment(coinValue, coinCurrency, coinType));
+			
+	}
+	// unknown Euro coin value (cents)
+	@Test
+	public void shouldRejectIllegalEuroCoin2() throws IllegalCoinException {
+		//Arrange	
+		int coinValue = 30;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.FRACTION;
+				
+		// Assert
+		Exception exception = assertThrows(IllegalCoinException.class, () ->
+			// Act	
+			ps.addPayment(coinValue, coinCurrency, coinType));
+				
+		}
+	
+	// unknown DKK coin value
+	@Test
+	public void shouldRejectIllegalDkkCoin() throws IllegalCoinException {
+		//Arrange	
+		int coinValue = 30;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.DKK;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+			
+		// Assert
+		Exception exception = assertThrows(IllegalCoinException.class, () ->
+			// Act	
+			ps.addPayment(coinValue, coinCurrency, coinType));
+			
+	}
+	// unknown DKK coin value (ore)
+		@Test
+		public void shouldRejectIllegalDkkCoin2() throws IllegalCoinException {
+			//Arrange	
+			int coinValue = 30;
+			Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.DKK;
+			Currency.ValidCoinType coinType = Currency.ValidCoinType.FRACTION;
+				
+			// Assert
+			Exception exception = assertThrows(IllegalCoinException.class, () ->
+				// Act	
+				ps.addPayment(coinValue, coinCurrency, coinType));
+				
+		}
+	
+	
+	
+	
 }
