2798f957711cc2ecedf58dc32e9bfcef9b8c682f
RodioserPL <61694890+RodioserPL@users.noreply.github.com>
2020-03-03 12:45:05 +0100

    Add files via upload

---
+++ utility/Calculation.java
@@ -0,0 +1,43 @@
+package utility;
+
+import modellayer.Coin;
+import modellayer.Currency;
+import modellayer.PPrice;
+
+public class Calculation {
+	public static int getTimeBoughtInMin(double amount) {
+		PPrice aPrice = new PPrice();
+		int timeBoughtInMinutes = 0;
+
+		double timeBoughtInSeconds = amount * aPrice.getParkingPrice();
+		timeBoughtInMinutes = (int) ((timeBoughtInSeconds + 59) / 60);
+		return timeBoughtInMinutes;
+	}
+	
+	public static double getEuroCoinValueInCent(Coin coin) {
+		double valueInCent = 0;
+		double coinValue = coin.getAmount();
+
+		if (coin.getCoinType() == Currency.ValidCoinType.INTEGER) {
+			valueInCent = coinValue * 100;
+		} else {
+			valueInCent = coinValue;
+		}
+
+		return valueInCent;
+	}
+	
+	public static double getDkkCoinValueInCent(Coin coin, PPrice price) {
+		double valueInCent = 0;
+		Currency.ValidCoinType coinType = coin.getCoinType();
+		double coinValue = coin.getAmount();
+
+		if (coinType == Currency.ValidCoinType.INTEGER) {
+			valueInCent = (coinValue * 100) / price.getExchangeEuroDkk();
+		} else {
+			valueInCent = coinValue / price.getExchangeEuroDkk();
+		}
+
+		return valueInCent;
+	}	
+}
